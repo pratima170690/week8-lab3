a81e77df4638d0fc0bb455f8d3e565eb34115102
pratima170690 <pratima.NA@metropolia.fi>
2024-04-03 11:54:28 +0300

    lab 1 finished

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const methodOverride = require('method-override')
+
+const connectDB = require("./config/db");
+
+const blogAPI = require("./controllers/messageAPIController");
+const blogSSR = require("./controllers/messageSSRController");
+
+//Important: will be discussed next week
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//https://expressjs.com/en/resources/middleware/method-override.html
+app.use(methodOverride('_method'))
+
+// Set views directory for EJS templates
+app.set("views", "views");
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+// Serve static files from the "public" directory
+app.use(express.static("public"));
+
+// Connect to MongoDB
+connectDB();
+
+// SSR
+// Route to render index.html with messages using EJS
+app.get("/", blogSSR.renderMessages);
+// Define a route to render the addmessage.ejs view
+app.get("/addmessage", blogSSR.renderForm);
+// Route to add  message using EJ
+app.post("/addmessage", blogSSR.addMessage);
+// Define a route to render the singlemessage.ejs view
+app.get("/single-message/:id", blogSSR.renderMessage);
+
+// API
+// GET all Messages
+app.get("/api/messages", blogAPI.getMessages);
+// POST a new Message
+app.post("/api/messages", blogAPI.addMessage);
+// GET a single Message
+app.get("/api/messages/:id", blogAPI.getMessage);
+// Update Message using PUT
+app.put("/api/messages/:id", blogAPI.updateMessage);
+// DELETE a Message
+app.delete("/api/messages/:id", blogAPI.deleteMessage);
+// DELETE all Message
+app.delete("/api/messages", blogAPI.deleteAllMessages);
+
+const PORT = 4000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
---
+++ controllers/messageSSRController.js
@@ -0,0 +1,62 @@
+const Message = require("../models/messageModel");
+
+// Render Controller: Render index.html with messages using EJS
+const renderMessages = async (req, res) => {
+  try {
+    const messages = await Message.find({});
+    console.log(messages)
+    res.render("index", { messages }); // Render index.ejs with messages data
+  } catch (error) {
+    console.error("Error rendering index.html:", error);
+    res.status(500).render("error");
+  }
+};
+ 
+// Get Message by ID
+const renderMessage = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const message = await Message.findById(id);
+    if (!message) {
+      return res.render("notfound");
+    }
+    res.render("singlemessage", { message }); // Render index.ejs with 
+  } catch (error) {
+    console.error("Error rendering Message:", error);
+    res.status(500).render("error");
+  }
+};
+ 
+const renderForm = (req, res) => {
+  try {
+    res.render("addmessage"); // Assuming "addmessage.ejs" is located in the "views" directory
+  } catch (error) {
+    console.error("Error rendering form", error);
+    res.status(500).render("error");
+  }
+};
+
+// Controller function to handle adding a new message (used for rendering and API)
+const addMessage = async (req, res) => {
+  try {
+    const { sender, recipient, content } = req.body;
+    // Convert the achieved field to a Boolean
+    // const achieved = req.body.achieved === "on";
+    const newMessage = new Message({ sender, recipient, content });
+    await newMessage.save();
+    // Redirect to the main page after successfully adding the message
+    console.log("Message added successfully");
+    res.redirect("/"); // Adjust the URL as needed
+  } catch (error) {
+    console.error("Error adding message:", error);
+    res.status(500).render("error");
+  }
+};
+
+
+module.exports = {
+  renderMessages,
+  renderMessage,
+  addMessage,
+  renderForm,
+};
